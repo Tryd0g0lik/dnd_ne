86401c51924d16876a44df3415e7329453e1ff22
Tryd0g0lik <work80@mail.ru>
2023-08-03 16:45:37 +0700

    v2: function for changing box location is complete

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -56,39 +56,16 @@ let boxTop: number;
 let clientX: number;
 let clientY: number;
 
-
 const onMouseOver = (e: MouseEvent) => {
   /**
    *there is the active box locationing dinding inder course(mouse)
    */
-
-  heightForNewLocation(actualCells)
-    .then((result) => {
-      Array.from(result).forEach((cell) => {
-        cell.addEventListener('mouseleave', handlerMouseLeave);
-        document.documentElement.addEventListener('mouseup', handlerMouseUP);
-
-        function handlerMouseUP(e: MouseEvent) {
-          Array.from(result).forEach((cell) => { if (cell.hasAttribute('style')) cell.removeAttribute('style'); });
-          document.body.removeEventListener('mouseup', handlerMouseUP);
-          e.stopPropagation();
-        }
-
-        function handlerMouseLeave(e: MouseEvent) {
-          if (cell.hasAttribute('style')) cell.removeAttribute('style');
-          document.documentElement.removeEventListener('mouseleave', handlerMouseLeave);
-          e.stopPropagation();
-        }
-      });
-      return result
-    });
-
   actualCells.style.top = e.clientY - (clientY - boxTop) + 'px'; // 28 это кнопка "удалить ячейку"
   actualCells.style.left = e.clientX - (clientX - boxLeft) + 'px';
-
-
-
+  (e.target as HTMLElement).removeEventListener('mouseenter', heightForNewLocation);
+  document.body.addEventListener('mouseleave', handleRemoveAttributePaddingTop, true)
 }
+
 
 const onMouseUp = (e: MouseEvent) => {
   const eventTarget = e.target as HTMLElement;
@@ -99,68 +76,56 @@ const onMouseUp = (e: MouseEvent) => {
      */
     actualColumn = eventTarget.parentElement as HTMLElement;
     actualColumn.insertBefore(actualCells, eventTarget);
-  }
+  }  
 
   actualCells.classList.remove('draggend');
   actualCells.removeAttribute('style');
-  actualCells = undefined as any;
 
-  Array.from(document.getElementsByClassName('task')).forEach((elem) => {
-    if (elem.hasAttribute('style')) elem.removeAttribute('style');
-  });
   document.documentElement.removeEventListener('mouseup', onMouseUp);
   document.documentElement.removeEventListener('mouseover', onMouseOver);
-  // document.documentElement.removeEventListener('mouseenter', () => heightForNewLocation);
-  // document.documentElement.removeEventListener('mouseleave', () => heightForNewLocation);
-
-
-
 
   (boxLeft as any) = undefined;
   (boxTop as any) = undefined;
   (clientX as any) = undefined;
   (clientY as any) = undefined;
+  actualCells = undefined as any;
   // The finished to the DnD actions
 }
 
-const heightForNewLocation = async (elem: HTMLElement) => {
 
-  let cells = await document.documentElement.getElementsByClassName('task') as HTMLCollectionOf<HTMLElement>;
+async function heightForNewLocation(e: MouseEvent) {
   /**
-   * this 'elem'  it's a new html-element which has the class 'draggend' at the now time.
+   * This's Handle for a event
    */
-  const result = await Array.from(cells).filter((cell) => {
-    if (cell.classList.contains('draggend') === false) return cell;
+  const response = (e.target as HTMLElement);
+
+  if (actualCells !== undefined
+    && response.classList.contains('task')
+    && response.classList.contains('draggend') === false) {
+    let box = response.getBoundingClientRect();
+
+    if (e.clientY >= box.top && e.clientY <= box.bottom
+      && e.clientX >= box.left && e.clientX <= box.right) {
+      const r = e.target as HTMLElement;
+      r.style.paddingTop = String(actualCells.offsetHeight) + 'px';
+    };
+  }
+
+}
+
+
+async function handleRemoveAttributePaddingTop() {
+  /**
+   * This's Handle for a event
+   */
+  Array.from(document.getElementsByClassName('task')).forEach((elem) => {
+    if (elem.hasAttribute('style')) elem.removeAttribute('style');
+    elem.removeEventListener('mouseleave', handleRemoveAttributePaddingTop);
   });
-
-  // for (let i = 0; i < cells.length; i++) {
-
-  Array.from(result).forEach((cell) => {
-    cell.addEventListener('mouseenter', heandlerMouseenter as any);
-
-    function heandlerMouseenter(e: MouseEvent) {
-      let box = cell.getBoundingClientRect();
-      // let r = e.target as HTMLElement;
-
-      if (e.clientY >= box.top && e.clientY <= box.bottom
-        && e.clientX >= box.left && e.clientX <= box.right) {
-        const r = e.target as HTMLElement;
-        r.style.marginTop = String(elem.offsetHeight) + 'px';
-
-      };
-      // document.documentElement.removeEventListener('mouseenter', heightForNewLocation as any);
-      document.documentElement.removeEventListener('mouseenter', heandlerMouseenter as any);
-      e.stopPropagation();
+}
 
 
-    }
-  });
-
-  return result
-};
-
-
-export function mouseEvents(elem: HTMLElement, e: MouseEvent) {
+function mouseEvents(elem: HTMLElement, e: MouseEvent) {
   /**
  * Start DnD action
  */
@@ -176,10 +141,21 @@ export function mouseEvents(elem: HTMLElement, e: MouseEvent) {
   elem.classList.add('draggend');
   actualCells = elem;
 
-
-
+  document.body.addEventListener('mouseenter', heightForNewLocation, true);
   document.documentElement.addEventListener('mouseup', onMouseUp);
   document.documentElement.addEventListener('mouseover', onMouseOver);
-
-
 }
+
+
+export function hadlerMmouseEvent(e: MouseEvent) {
+  /**
+* This's headler for a 'mousedown' MouseEvent
+*/
+
+  if ((e.target as HTMLElement).classList.contains('task')) {
+    const cell = e.target as HTMLElement;
+    e.preventDefault();
+    mouseEvents(cell, e);
+  }
+  document.documentElement.removeEventListener('mousedown', hadlerMmouseEvent);
+} 
